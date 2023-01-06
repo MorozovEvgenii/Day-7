dd63c1f0a8325b533a3825b79adb014952a482e9
Morozov_Evgenii <121439089+MorozovEvgenii@users.noreply.github.com>
2023-01-06 17:43:03 +0600

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <ctime>
+#include <set>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	// Создание переменной хранящей случайное значение
+	srand(time(NULL));
+
+	// Объявление мультисета
+	multiset <int> MST;
+
+	// Добавление случайных значений в мультисет
+	cout << "Объявление случайных значений: " << endl;
+	for (int i = 1; i < 15; i++) {
+		int random = rand() % 10 + 2;
+		MST.insert(random);
+		cout << i + 1 << ") " << random << endl;
+	}
+
+	multiset <int> ::iterator it = MST.begin();
+
+	// Вывод элементов из мультисета
+	cout << "Отсортированный вариант -  " << endl;
+	for (int i = 1; it != MST.end(); i++, it++) {
+		cout << *it << " ";
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ ConsoleApplication1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+	setlocale(LC_ALL, "ru-RU");
+
+	std::cout << "Первое число -  " << (rand() % 100 + 51) << "\n";
+	srand(time(NULL));
+	std::cout << "Случайное число - " << (rand() % 100) << "\n";
+	srand(1);
+	std::cout << "Число 3 - " << (rand() % 100 + 20) << "\n";
+
+	return 0;
+}
